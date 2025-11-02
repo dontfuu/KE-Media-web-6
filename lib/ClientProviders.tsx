8d3599bac749b9ffc4c3e1988483a7097fd2ed4d
dontfuu <phucnguyendong6603@gmail.com>
2025-11-03 00:04:51 +0700

    Update ClientProviders.tsx

--- lib/ClientProviders.tsx
+++ lib/ClientProviders.tsx
@@ -1,1 +1,12 @@
+'use client'
+
+import { LanguageProvider } from './language'
+
+export default function ClientProviders({ children }: { children: React.ReactNode }) {
+  return (
+    <LanguageProvider>
+      {children}
+    </LanguageProvider>
+  )
+}
 
