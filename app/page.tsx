79ac83e4791560434c6d148dd2c74e4606f9071b
dontfuu <phucnguyendong6603@gmail.com>
2025-11-02 21:47:01 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Hero from '@/components/Hero'
-import Services from '@/components/Services'
-import Portfolio from '@/components/Portfolio'
-import ClientLogos from '@/components/ClientLogos'
-import Stats from '@/components/Stats'
-import About from '@/components/About'
-import CTA from '@/components/CTA'
+import Hero from '../components/Hero'
+import Services from '../components/Services'
+import Portfolio from '../components/Portfolio'
+import ClientLogos from '../components/ClientLogos'
+import Stats from '../components/Stats'
+import About from '../components/About'
+import CTA from '../components/CTA'
 
 export default function Home() {
   return (
