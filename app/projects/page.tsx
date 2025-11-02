f520e223f689bfb3b35701cbc54f2b5edfa2938f
dontfuu <phucnguyendong6603@gmail.com>
2025-11-02 21:49:58 +0700

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { useLanguage } from '../lib/language'
+import { useLanguage } from '../../lib/language'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -129,4 +129,5 @@ export default function ProjectsPage() {
     </div>
   )
 }
+
 
